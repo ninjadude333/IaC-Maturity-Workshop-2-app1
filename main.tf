74fa2896d280f90bf606592649603f7f98447345
David Gidony <david.gidony@gmail.com>
2023-01-05 15:01:43 +0200

    testing

--- main.tf
+++ main.tf
@@ -11,16 +11,16 @@ data "terraform_remote_state" "vpc" {
 }
 
 module "terraform-AWS-Maturity_Workshop_App_module" {
-  source              = "./modules/terraform-AWS-Maturity_Workshop_App_module"
-  
-  app_name            = var.App_Name
-  vpc_name            = data.terraform_remote_state.vpc.outputs.VPC_NAME
-  vpc_id              = data.terraform_remote_state.vpc.outputs.VPC_ID
-  aws_region          = var.aws_region
-  vpc_cidr            = data.terraform_remote_state.vpc.outputs.VPC_CIDR
-  vpc_private_subnets = data.terraform_remote_state.vpc.outputs.VPC_Private_Subnets
-  vpc_public_subnet_ids  = data.terraform_remote_state.vpc.outputs.VPC_Public_Subnets
-  keypair_name        = var.key_pair_Name
-  aws_access_key = var.aws_access_key
-  aws_aws_secret_key =  var.aws_secret_key
+  source = "./modules/terraform-AWS-Maturity_Workshop_App_module"
+
+  app_name              = var.App_Name
+  vpc_name              = data.terraform_remote_state.vpc.outputs.VPC_NAME
+  vpc_id                = data.terraform_remote_state.vpc.outputs.VPC_ID
+  aws_region            = var.aws_region
+  vpc_cidr              = data.terraform_remote_state.vpc.outputs.VPC_CIDR
+  vpc_private_subnets   = data.terraform_remote_state.vpc.outputs.VPC_Private_Subnets
+  vpc_public_subnet_ids = data.terraform_remote_state.vpc.outputs.VPC_Public_Subnets
+  keypair_name          = var.key_pair_Name
+  aws_access_key        = var.aws_access_key
+  aws_secret_key        = var.aws_secret_key
 }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
- type = string
+  type = string
 }
 
 variable "aws_secret_key" {
- type = string
+  type = string
 }
 
 variable "TF_HCP_org_name" {
