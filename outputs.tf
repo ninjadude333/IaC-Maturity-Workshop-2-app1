e0cd16e69fe55d1086ae5b6b0c844f92f03e1d4e
David Gidony <david.gidony@gmail.com>
2023-01-05 14:52:13 +0200

    testing

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "APP-LoadBalancer-dns" {
   description = "Alb dns name"
-  value       = module.terraform-AWS-Maturity_Workshop_App_module.outputs.APP-LoadBalancer-dns
+  value       = module.terraform-AWS-Maturity_Workshop_App_module.output.APP-LoadBalancer-dns
 }
 
 output "private_key" {
-  value     = module.terraform-AWS-Maturity_Workshop_App_module.outputs.private_key
+  value     = module.terraform-AWS-Maturity_Workshop_App_module.output.private_key
   sensitive = true
 }
