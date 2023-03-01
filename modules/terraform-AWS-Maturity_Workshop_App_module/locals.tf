8221326dae257d83fda603a2cfaa15088d1a0e8d
David Gidony <david.gidony@gmail.com>
2023-03-01 09:08:42 +0200

    tag update

--- modules/terraform-AWS-Maturity_Workshop_App_module/locals.tf
+++ modules/terraform-AWS-Maturity_Workshop_App_module/locals.tf
@@ -1,9 +1,13 @@
 locals {
   vpc_azs = ["${var.aws_region}a", "${var.aws_region}b", "${var.aws_region}c"]
+  timestamp = timestamp()
   tags = {
     Owner     = "David Gidony"
     Env       = "Maturity-Workshop"
     Terraform = "True"
     Phase     = var.Phase
+    Keep            = "True"
+    "IAM User Name" = "davidg@terasky.com"
+    "Creation Date" = "${formatdate("YYYY-MM-DD hh:mm:ss", local.timestamp)}"
   }
 }
